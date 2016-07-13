05ea917b238926a83e6326f7aca4c681e81cd28e
rahul-msRnD <rahulb246@outlook.com>
2016-07-14 02:39:41 +0530

    updated display

--- blogPosts/forms.py
+++ blogPosts/forms.py
@@ -7,7 +7,6 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
     content = forms.CharField(widget=PagedownWidget(show_preview=False))
-    publish = forms.DateField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
         fields = [
